edcdb6a761a790a7a29bbba3b6555b86847b2d6a
Matheus Alexandre <mathe.system@gmail.com>
2021-06-05 11:11:56 -0300

    feat: add login view

--- webpack.config.js
+++ webpack.config.js
@@ -16,22 +16,17 @@ module.exports = {
     }
   },
   module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader', options: { modules: true } },
-          { loader: 'style-loader' }
-        ],
-        exclude: /node_modules/
-      }
-    ]
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [
+        { loader: 'style-loader' },
+        { loader: 'css-loader', options: { modules: true } },
+        { loader: 'sass-loader' }]
+    }]
   },
   devServer: {
     contentBase: './public',
