4da28ccb54bdeb307e22085690e2525d3cb92d83
Matheus Alexandre <mathe.system@gmail.com>
2021-06-08 21:50:54 -0300

    feat: ensure Login present error if passowrd is invalid

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -21,14 +21,11 @@ const Login: FC<Props> = ({ validation }: Props) => {
   useEffect(() => {
     setState({
       ...state,
-      emailError: validation.validate('email', state.email)
+      emailError: validation.validate('email', state.email),
+      passwordError: validation.validate('password', state.password)
 
     })
-  }, [state.email])
-
-  useEffect(() => {
-    validation.validate('password', state.password)
-  }, [state.password])
+  }, [state.email, state.password])
 
   return (
     <div className={Styles.login}>
