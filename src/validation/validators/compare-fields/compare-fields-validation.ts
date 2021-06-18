cd175c1d244a7bcc94ee6a30ba52384776f04663
Matheus Alexandre <mathe.system@gmail.com>
2021-06-18 18:08:00 -0300

    feat: ensure CompareFieldsValidation returns falsy if validation succeeds

--- src/validation/validators/compare-fields/compare-fields-validation.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -4,7 +4,7 @@ import { InvalidFieldError } from '@/validation/errors'
 export class CompareFieldsValidation implements FieldValidation {
   constructor (readonly field: string, private readonly valueToCompare: string) {}
 
-  validate (value): Error {
-    return new InvalidFieldError(this.field)
+  validate (value: string): Error {
+    return value !== this.valueToCompare ? new InvalidFieldError(this.field) : null
   }
 }
--- src/validation/validators/required-field/required-field-validation.ts
+++ src/validation/validators/required-field/required-field-validation.ts
@@ -4,7 +4,7 @@ import { RequiredFieldError } from '@/validation/errors'
 export class RequiredFieldValidation implements FieldValidation {
   constructor (readonly field: string) {}
 
-  validate (value): Error {
+  validate (value: string): Error {
     return value ? null : new RequiredFieldError()
   }
 }
