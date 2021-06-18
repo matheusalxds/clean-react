131ec37fd237dc90ccfc89073efefe07c55bce03
Matheus Alexandre <mathe.system@gmail.com>
2021-06-18 14:01:43 -0300

    refactor: rename SetStorageSpy to SetStorageMock

--- src/data/test/mock-storage.ts
+++ src/data/test/mock-storage.ts
@@ -1,6 +1,12 @@
 import { SetStorage } from '@/data/protocols/cache'
 
-export class SetStorageSpy implements SetStorage {
+/**
+ * Stub don't need to capture the values, it uses classes return some value
+ * Spy capture values and mock a value
+ * Mock doesn't have returns, only receive values
+ */
+
+export class SetStorageMock implements SetStorage {
   key: string
   value: any
 
