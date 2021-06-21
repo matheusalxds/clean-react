33c936bf0a915f71dc3fe70adba8e1dc9ac0e526
Matheus Alexandre <mathe.system@gmail.com>
2021-06-21 15:31:11 -0300

    feat: ensure SignUp calls AddAccount with correct values

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -5,12 +5,14 @@ import Context from '@/presentation/contexts/form/form-context'
 
 import Styles from './signup-styles.scss'
 import { Validation } from '@/presentation/protocols/validation'
+import { AddAccount } from '@/domain/usecases'
 
 type Props = {
   validation: Validation
+  addAccount: AddAccount
 }
 
-const Signup: FC<Props> = ({ validation }: Props) => {
+const Signup: FC<Props> = ({ validation, addAccount }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     email: '',
@@ -37,6 +39,12 @@ const Signup: FC<Props> = ({ validation }: Props) => {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
     setState({ ...state, isLoading: true })
+    await addAccount.add({
+      name: state.name,
+      email: state.email,
+      password: state.password,
+      passwordConfirmation: state.passwordConfirmation
+    })
   }
 
   return (
