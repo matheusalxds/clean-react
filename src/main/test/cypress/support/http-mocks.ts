a0e8dae9ac970d8c4394acd959419f2d55bad418
Matheus Alexandre <mathe.system@gmail.com>
2021-06-22 19:49:31 -0300

    refactor: move duplicated code to helper methods

---
+++ src/main/test/cypress/support/http-mocks.ts
@@ -0,0 +1,36 @@
+import faker from 'faker'
+
+const aliasName = 'request'
+export const mockInvalidCredentialsError = (url: RegExp): void => {
+  cy.server()
+  cy.route({
+    method: 'POST',
+    url,
+    status: 401,
+    response: {
+      error: faker.random.words()
+    }
+  }).as(aliasName)
+}
+
+export const mockUnexpectedErrorError = (url: RegExp, method: string): void => {
+  cy.server()
+  cy.route({
+    method,
+    url,
+    status: faker.helpers.randomize([400, 404, 500]),
+    response: {
+      error: faker.random.words()
+    }
+  }).as(aliasName)
+}
+
+export const mockOk = (url: RegExp, method: string, response: any): void => {
+  cy.server()
+  cy.route({
+    method,
+    url,
+    status: 200,
+    response
+  }).as(aliasName)
+}
