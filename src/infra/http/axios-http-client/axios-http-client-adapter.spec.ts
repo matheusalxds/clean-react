d85130b3866cd71984b7606105f4a2ebea7f6d4a
Matheus Alexandre <mathe.system@gmail.com>
2021-07-03 16:21:10 -0300

    test: ensure AxiosHttpClient returns correct value on failureclear

--- src/infra/http/axios-http-client/axios-http-client-adapter.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client-adapter.spec.ts
@@ -66,5 +66,14 @@ describe('AxiosHttpClient', () => {
         body: axiosResponse.data
       })
     })
+
+    test('should return correct error on axios.post', () => {
+      const { sut, mockedAxios } = makeSut()
+      mockedAxios.get.mockRejectedValueOnce({
+        response: mockHttpResponse()
+      })
+      const promise = sut.get(mockGetRequest())
+      expect(promise).toEqual(mockedAxios.get.mock.results[0].value)
+    })
   })
 })
