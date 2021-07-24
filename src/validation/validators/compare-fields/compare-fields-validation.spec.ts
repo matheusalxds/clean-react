17855ba7d0189bf2cdd813ee0657c04a5582c996
Matheus Alexandre <mathe.system@gmail.com>
2021-07-24 09:34:44 -0300

    fix: change CompareFieldsValidation test to never returns equal values

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -19,7 +19,7 @@ describe('RequiredFieldValidation', () => {
     const field = faker.database.column()
     const fieldToCompare = faker.database.column()
     const { sut } = makeSut(field, fieldToCompare)
-    const error = sut.validate({ [field]: faker.random.word(), [fieldToCompare]: faker.random.word() })
+    const error = sut.validate({ [field]: faker.random.words(3), [fieldToCompare]: faker.random.words(4) })
     expect(error).toEqual(new InvalidFieldError(field))
   })
 
