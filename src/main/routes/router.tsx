d14f3d065bc9d81918e2764ed0f977453020c609
Matheus Alexandre <mathe.system@gmail.com>
2021-09-07 19:53:33 -0300

    feat: ensure PrivateRoute renders component if token is not empty

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,23 +1,28 @@
-import React, { FC } from 'react'
+import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
 import { makeLogin } from '@/main/factories/pages/login/login-factory'
 import { makeSignUp } from '@/main/factories/pages/signup/signup-factory'
-import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '@/main/adapters/current-account-adapter'
-import { SurveyList } from '@/presentation/pages'
+import { setCurrentAccountAdapter, getCurrentAccountAdapter } from '@/main/adapters/current-account-adapter'
 import { ApiContext } from '@/presentation/contexts'
-import { PrivateRoute } from '@/presentation/components'
+import { SurveyList } from '@/presentation/pages'
 
-const Router: FC = () => (
-  <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountAdapter, getCurrentAccount: getCurrentAccountAdapter }}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/login" exact component={makeLogin}/>
-        <Route path="/signup" exact component={makeSignUp}/>
-        <PrivateRoute path="/" exact component={SurveyList} />
-      </Switch>
-    </BrowserRouter>
-  </ApiContext.Provider>
-)
+const Router: React.FC = () => {
+  return (
+    <ApiContext.Provider
+      value={{
+        setCurrentAccount: setCurrentAccountAdapter,
+        getCurrentAccount: getCurrentAccountAdapter
+      }}
+    >
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" exact component={makeLogin} />
+          <Route path="/signup" exact component={makeSignUp} />
+          <Route path="/" exact component={SurveyList} />
+        </Switch>
+      </BrowserRouter>
+    </ApiContext.Provider>
+  )
+}
 
 export default Router
