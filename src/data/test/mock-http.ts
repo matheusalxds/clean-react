46fefcf5c19765abd687f1cfc3f5eb0c52145a85
Matheus Alexandre <mathe.system@gmail.com>
2021-10-01 10:29:22 -0300

    test: ensure AxiosHttpClient calls Axios with correct values

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -18,8 +18,9 @@ export const mockPostRequest = (): HttpPostParams => ({
   url: faker.internet.url(),
   body: faker.random.objectElement()
 })
-export const mockGetRequest = (): HttpPostParams => ({
-  url: faker.internet.url()
+export const mockGetRequest = (): HttpGetParams => ({
+  url: faker.internet.url(),
+  headers: faker.random.objectElement()
 })
 
 export class HttpClientPostSpy<R> implements HttpPostClient<R> {
